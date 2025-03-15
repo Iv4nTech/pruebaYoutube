e4a7e6bc73159acec7d98bb2a77e6a44a5d23bd3
Iv4nTech <133955173+Iv4nTech@users.noreply.github.com>
2025-03-15 20:25:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("magicButton").addEventListener("click", function() {
     let mensaje = document.getElementById("mensaje");
-    mensaje.textContent = "¡Has activado la magia! ✨";
+    mensaje.textContent = "¡Te has sucrito al mejor canal de YouTube! 🙀";
     mensaje.style.opacity = 0;
     mensaje.style.transition = "opacity 1s";
     setTimeout(() => mensaje.style.opacity = 1, 100);
